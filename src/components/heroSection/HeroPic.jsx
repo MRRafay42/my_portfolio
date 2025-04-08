cd2fc4e6db97068719d4eae115faf8cf237dbea0
Mohammad rafay <rafayshabbir42@gmail.com>
2025-04-08 22:33:46 +0500

    change my image

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 import { PiHexagonThin } from "react-icons/pi";
+import HEImage from "../../assets/HE.png";
 
 const HeroPic = () => {
   return (
@@ -11,11 +12,7 @@ const HeroPic = () => {
       viewport={{ once: false, amount: 0 }}
       className=" h-full flex items-center justify-center "
     >
-      <img
-        src="../../public/images/HE.png"
-        alt="M rafay"
-        className="max-h-[350px] w-auto"
-      />
+      <img src={HEImage} alt="M rafay" className="max-h-[350px] w-auto" />
 
       <div className=" absolute flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[450px] w-auto text-cyan blur-sm animate-[spin_10s_linear_infinite] " />
